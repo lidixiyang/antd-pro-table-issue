d1ea6e8ca90f6562184a76db33a38d8d9c195e9c
Lidixiyang <13661097209@163.com>
2020-10-21 17:16:30 +0800

    first commit

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Card, Alert, Typography ,Table} from 'antd';
+import styles from './Welcome.less';
+
+const CodePreview: React.FC<{}> = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <div>{text}</div>,
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown John Brown ',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+  },
+];
+
+export default (): React.ReactNode => (
+  <PageContainer>
+    <Table columns={columns} dataSource={data} bordered={true} showHeader={false} pagination={false} style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}/>
+    <Card>
+      <Alert
+        message="更快更强的重型组件，已经发布。"
+        type="success"
+        showIcon
+        banner
+        style={{
+          margin: -12,
+          marginBottom: 24,
+        }}
+      />
+      <Typography.Text strong>
+        高级表格{' '}
+        <a href="https://protable.ant.design/" rel="noopener noreferrer" target="__blank">
+          欢迎使用
+        </a>
+      </Typography.Text>
+      <CodePreview>yarn add @ant-design/pro-table</CodePreview>
+      <Typography.Text
+        strong
+        style={{
+          marginBottom: 12,
+        }}
+      >
+        高级布局{' '}
+        <a href="https://prolayout.ant.design/" rel="noopener noreferrer" target="__blank">
+          欢迎使用
+        </a>
+      </Typography.Text>
+      <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
+    </Card>
+  </PageContainer>
+);
